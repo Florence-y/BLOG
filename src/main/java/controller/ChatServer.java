7c65de7352306a51be999bc288f8e320a0509ce8
Florence_y <2213576511@qq.com>
2020-09-29 21:08:48 +0800

    websocket and elasticSearch

--- src/main/java/controller/ChatServer.java
+++ src/main/java/controller/ChatServer.java
@@ -1,9 +1,12 @@
 package controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
--- src/main/java/test/ElasticUtilTest.java
+++ src/main/java/test/ElasticUtilTest.java
@@ -1,15 +1,54 @@
 package test;
 
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+import org.elasticsearch.cluster.metadata.MappingMetadata;
 import org.junit.Test;
 import until.ElasticUtil;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
+/**
+ * @author Florence es测试类
+ */
 public class ElasticUtilTest {
+    RestHighLevelClient restHighLevelClient = ElasticUtil.getRestHighLevelClient();
     @Test
-    public void testGetConnection(){
-        RestHighLevelClient restHighLevelClient = ElasticUtil.getRestHighLevelClient();
-        System.out.println();
+    public void testGetConnection() {
+        try {
+            Map<String, Object> message = new HashMap<>();
+            message.put("type", "text");
+            Map<String, Object> properties = new HashMap<>();
+            properties.put("message", message);
+            Map<String, Object> mapping = new HashMap<>();
+            mapping.put("properties", properties);
+
+            CreateIndexRequest request = new CreateIndexRequest("test");
+            request.mapping(mapping);
+            CreateIndexResponse createIndexResponse = restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
+            System.out.println(createIndexResponse.isAcknowledged());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void getIndexTest(){
+        try {
+            GetIndexRequest request = new GetIndexRequest("test");
+            GetIndexResponse getIndexResponse = restHighLevelClient.indices().get(request, RequestOptions.DEFAULT);
+            Map<String, MappingMetadata> mappings = getIndexResponse.getMappings();
+            MappingMetadata mappingMetadata =mappings.get("test");
+            System.out.println("Asdadad");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
